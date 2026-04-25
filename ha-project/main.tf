da882e27589ddf9802899bad4269c615bff09112
AleksTheBorsh <drumadu@gmail.com>
2026-04-25 04:51:03 +0000

    fix folder_id in main.tf

--- ha-project/main.tf
+++ ha-project/main.tf
@@ -34,7 +34,7 @@ resource "yandex_iam_service_account" "ig_sa" {
 
 # 6. Даем ему роль "editor", чтобы он мог создавать серверы
 resource "yandex_resourcemanager_folder_iam_member" "ig_sa_editor" {
-  folder_id = var.folder_id 
+  folder_id = var.folder_id
   role      = "editor"
   member    = "serviceAccount:${yandex_iam_service_account.ig_sa.id}"
 }
