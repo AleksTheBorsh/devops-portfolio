f6a9bceb1f15a701409b564fba31786cdf2c1bac
AleksTheBorsh <drumadu@gmail.com>
2026-05-02 08:30:00 +0000

    fix init-script

--- terraform-project/init-script.sh
+++ terraform-project/init-script.sh
@@ -3,23 +3,27 @@
 # 1. Ставим зависимости и Docker
 apt update && apt install -y curl wget apt-transport-https docker.io
 
-# 2. Даем права пользователю aleks (чтобы ты мог управлять докером по SSH)
-usermod -aG docker aleks
+# 2. Даем права пользователю ubuntu (чтобы ты мог управлять докером по SSH)
+usermod -aG docker ubuntu
 chmod 666 /var/run/docker.sock
+
+sleep 30
 
 # 3. Скачиваем и ставим Minikube (разделено на две строки!)
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube
 
+sleep 15
+
 # 4. Скачиваем и ставим kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install kubectl /usr/local/bin/kubectl
 
-# 5. МАГИЯ: Запускаем кластер от имени обычного юзера aleks
-su - aleks -c "minikube start --driver=docker"
+# 5.  Запускаем кластер от имени обычного юзера ubuntu
+su - ubuntu -c "minikube start --driver=docker"
 
-# 6. Создаем манифест Nginx (сразу в домашней папке пользователя aleks)
-cat <<EOF > /home/aleks/nginx-pod.yaml
+# 6. Создаем манифест Nginx (сразу в домашней папке пользователя ubuntu)
+cat <<EOF > /home/ubuntu/nginx-pod.yaml
 apiVersion: v1
 kind: Pod
 metadata:   
@@ -32,5 +36,5 @@ spec:
     - containerPort: 80
 EOF
 
-# 7. Применяем манифест (тоже от имени aleks)
-su - aleks -c "kubectl apply -f /home/aleks/nginx-pod.yaml"
+# 7. Применяем манифест (тоже от имени ubuntu)
+su - ubuntu -c "kubectl apply -f /home/ubuntu/nginx-pod.yaml"
